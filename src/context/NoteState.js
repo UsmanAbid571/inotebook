d4b50d258b2acc7f4eefb51035f23c5fc11d0bf4
Usman <muhammad.usmanabid571@gmail.com>
2024-06-28 18:33:48 +0500

    vercel url

--- src/context/NoteState.js
+++ src/context/NoteState.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 const NoteState = (props) => {
     //Host URL here
-    const host = "http://localhost:5000"
+    const host = "https://inotebook-backend1.vercel.app"
 
     const notesInitial = []
     const [notes, setNotes] = useState(notesInitial)
